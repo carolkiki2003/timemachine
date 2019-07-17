d3c96b95257aaf926409a7fcb9559f4cd48134d5
carol.yan <>
2019-07-17 18:55:59 +0800

    第一版

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,21 @@
+<script>
+  // @ is an alias to /src
+  import TripStartContainer from '@/containers/TripStartContainer'
+/* eslint-disable */
+  export default {
+    name: 'home',
+    data() {
+      return {
+      }
+    },
+    components: {
+       TripStartContainer 
+    },
+    computed: {
+    },
+    methods: {
+    }
+  }
+</script>
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped />
