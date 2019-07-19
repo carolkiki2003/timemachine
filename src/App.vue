f68e06accf15e2842d2c681f7d3ad363235dda76
carol <carolkiki2003@gmail.com>
2019-07-20 00:18:47 +0800

    修改font-family

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
-    <Home/>
+    <Home />
   </div>
 </template>
 
 <script>
-import Home from '@/pages/Home'
-export default {
-  components: {
+  import Home from '@/pages/Home'
+  export default {
+    components: {
       Home
     }
-}
+  }
 </script>
 
 <style>
-@import './assets/normalize.scss';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import './assets/normalize.scss';
+  #app {
+    font-family: 'Microsoft JhengHei', 'Apple LiGothic Medium', 'PMingLiU', 'sans-serif', 'serif';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
